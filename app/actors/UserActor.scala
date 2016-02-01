bf494470954064ab76a9bb4db36f429b954b3fd2
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-02-01 15:57:14 +0900

    tmp

--- app/actors/UserActor.scala
+++ app/actors/UserActor.scala
@@ -1,6 +1,8 @@
 package actors
 
-import akka.actor.{Actor, ActorRef, Props}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
+import akka.actor.{Actor, ActorRef, Props, ActorLogging}
 import play.api.libs.json.{Json, JsValue, Writes}
 import models.Question
 import UserActor._
@@ -27,12 +29,29 @@ object UserActor {
   }
 }
 
-class UserActor(uid: UID, field: ActorRef, out: ActorRef) extends Actor {
+class UserActor(uid: UID, field: ActorRef, out: ActorRef) extends Actor with ActorLogging {
+  val cluster = Cluster(context.system)
+
   override def preStart() = {
+    println("preStart")
+    println(cluster)
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents, classOf[MemberEvent], classOf[UnreachableMember])
+    println(cluster)
     FieldActor.field ! FieldActor.Subscribe(uid)
   }
 
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
   def receive = {
+    case MemberUp(member) =>
+      println("up")
+      log.info("Member is Up: {}", member.address)
+    case UnreachableMember(member) =>
+      println("unreachable")
+      log.info("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) =>
+      println("remove")
+      log.info("Member is Removed: {} after {}", member.address, previousStatus)
     case js: JsValue => {
       (js \ "result").validate[Boolean] foreach { field ! FieldActor.Result(_) }
       val question = Json.obj("type" -> "question", "question" -> Question.create())
