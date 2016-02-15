763daf0fa959d8618540d155cd73322c831f4357
ozaki <fs65ugumori@yahoo.co.jp>
2016-02-15 21:45:03 +0900

    [#22]FieldActorをPersistActorにした

--- modules/user/src/main/scala/com/example/calcbattle/user/Main.scala
+++ modules/user/src/main/scala/com/example/calcbattle/user/Main.scala
@@ -18,9 +18,13 @@ object Main extends App {
         ).withFallback(ConfigFactory.load())
 
       val system = ActorSystem("application", config)
+
+      FieldActor.startupSharding(system)
       val field = system.actorOf(FieldActor.props, FieldActor.name)
+
       system.actorOf(UserActor.props, UserActor.name)
       UserActor.startupSharding(system, field)
+
       Await.result(system.whenTerminated, Duration.Inf)
     case _ =>
       throw new IllegalArgumentException("引数には <ホスト名> <ポート番号> を指定してください。")
--- modules/user/src/main/scala/com/example/calcbattle/user/actors/FieldActor.scala
+++ modules/user/src/main/scala/com/example/calcbattle/user/actors/FieldActor.scala
@@ -1,20 +1,51 @@
 package com.example.calcbattle.user.actors
 
-import akka.actor.{ActorRef, ActorLogging, Actor, Props, Terminated}
+import akka.actor._
+import akka.cluster.sharding.{ShardRegion, ClusterSharding, ClusterShardingSettings}
+import akka.persistence.PersistentActor
 
 object FieldActor {
   def props = Props(new FieldActor)
   val name = "FieldActor"
+  val nrOfShards = 50
+
+  val extractEntityId: ShardRegion.ExtractEntityId = {
+    case msg @ Join(uid) => (uid.id, msg)
+  }
+
+  val extractShardId: ShardRegion.ExtractShardId = {
+    case msg @ Join(uid) => (uid.hashCode % nrOfShards).toString
+  }
+
+  def startupSharding(system: ActorSystem) = {
+    ClusterSharding(system).start(
+      typeName = FieldActor.name,
+      entityProps = FieldActor.props,
+      settings = ClusterShardingSettings(system),
+      extractEntityId = extractEntityId,
+      extractShardId = extractShardId
+    )
+  }
 
   case class UID(val id: String) extends AnyVal
   case class Join(uid: UID)
   case class Participation(uids: Set[UID])
+
+  sealed trait Event
+  // [TODO]
 }
 
-class FieldActor extends Actor with ActorLogging {
+class FieldActor extends PersistentActor with ActorLogging {
   import akka.cluster.pubsub.DistributedPubSub
   import akka.cluster.pubsub.DistributedPubSubMediator.{Subscribe, SubscribeAck}
   import com.example.calcbattle.user.actors.FieldActor._
+
+  override def persistenceId: String = name
+
+  override def receiveRecover: Receive = {
+    case event: Event =>
+    // [TODO]
+  }
 
   override def preStart() = {
     val mediator = DistributedPubSub(context.system).mediator
@@ -23,7 +54,7 @@ class FieldActor extends Actor with ActorLogging {
 
   var users = Map[ActorRef, UID]()
 
-  def receive = {
+  def receiveCommand = {
     case Join(uid) =>
       users += (sender -> uid)
       context watch sender
