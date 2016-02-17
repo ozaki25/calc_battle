48557d16ba8af46c56626fc232ebeaf401493cce
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-02-17 17:25:47 +0900

    [#27]uidをjava.utils.UUIDを使うのに変更

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,22 +1,23 @@
 package controllers
 
+
 import play.api._
 import play.api.mvc._
 import play.api.Play.current
 import play.api.libs.json.JsValue
 
 import scala.concurrent.Future
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.UUID
+
 import actors.SocketActor
 import com.example.calcbattle.user.actors.FieldActor.UID
 
 class Application extends Controller {
   val UID = "uid"
-  val counter = new AtomicInteger()
 
   def index = Action { implicit request =>
     val uid: String = request.session.get(UID).getOrElse {
-      counter.incrementAndGet().toString()
+      UUID.randomUUID().toString
     }
     Ok(views.html.index(uid)).withSession {
       request.session + (UID -> uid)
